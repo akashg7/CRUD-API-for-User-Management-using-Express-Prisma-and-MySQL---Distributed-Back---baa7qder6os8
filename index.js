0ba0363c2d1a02ed364b33b3eafa044b5fcba8e8
akashg7 <146759434+akashg7@users.noreply.github.com>
2024-12-01 23:24:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.post('/api/auth/login', async (req, res) => {
       JWT_SECRET,
       { expiresIn: '1h' }
     );
-if(user){
+
     return res.status(200).json({
       userdata: {
         id: user.id,
@@ -89,7 +89,6 @@ if(user){
       },
       accessToken: token,
     });
-}
   } catch (err) {
     console.error(err);
     return res.status(500).json({ message: "Internal server error" });
